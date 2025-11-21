e8516844fe9ca99cab7db0078f91b343633d9aab
LeaFluorine <3377422475@qq.com>
2025-11-21 23:43:01 +0800

    On branch main  Changes to be committed: 	modified:   __init__.py 	renamed:    bot_status/__init__.py -> linglingbizui/__init__.py 	renamed:    bot_status/__pycache__/image_generator.cpython-312.pyc -> linglingbizui/__pycache__/image_generator.cpython-312.pyc 	renamed:    bot_status/generate_preview.py -> linglingbizui/generate_preview.py 	renamed:    bot_status/image_generator.py -> linglingbizui/image_generator.py 	renamed:    bot_status/plugin.py -> linglingbizui/plugin.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from src.plugin_system.base.plugin_metadata import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
-    name="BotStatus",
-    description="一个用于生成机器人和系统状态图的插件。",
+    name="linglingbizui",
+    description="一条指令让bot闭嘴闭眼",
     usage="/status",
     version="1.0.0",
-    author="YiShan",
+    author="LeaFluorine",
     license="GPL-v3.0-or-later",
     repository_url="https://github.com/minecraft1024a",
     keywords=["status", "statistics", "management"],
